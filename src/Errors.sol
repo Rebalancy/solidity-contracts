ae2effd9508421129b8d1c75cc9c3e7738c4bd80
edsonalcala <edson_alcala@msn.com>
2025-08-14 18:35:12 +0200

    added extra errors

--- src/Errors.sol
+++ src/Errors.sol
@@ -4,3 +4,8 @@ pragma solidity 0.8.28;
 error InvalidAmount();
 error MaximumInvestmentExceeded();
 error NotEnoughAssetsToInvest();
+error InvalidAgentAddress();
+error InvalidUnderlyingAddress();
+error InvalidAavePoolAddress();
+error InvalidATokenAddress();
+error NotEnoughAssetsToWithdraw();
